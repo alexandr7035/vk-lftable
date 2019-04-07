c0bbf022d8eadacb541227f85e994a37eb4f6795
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-04-07 11:41:54 +0000

    Test keyboard works.

--- vk-lftable.py
+++ vk-lftable.py
@@ -9,18 +9,68 @@ import sys
 import vk
 print(sys.argv)
 
-vk_api_version = 5.50
+vk_api_version = 5.90
 
 # Токены
 try:
-	from tokens import vk_token
+    from tokens import vk_token
 except Exception:
-	print("Can't load vk_token from file. Exit.")
+    print("Can't load vk_token from file. Exit.")
 
 try:
-	from tokens import confirmation_token
+    from tokens import confirmation_token
 except Exception:
-	print("Can't load confirmation_token from file. Exit.")
+    print("Can't load confirmation_token from file. Exit.")
+
+
+
+def menu_keyboard():
+	
+	
+	
+    keyb = {
+    "one_time": False,
+    "buttons": [
+      [{
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"1\"}",
+          "label": "Red"
+        },
+        "color": "negative"
+      },
+     {
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"2\"}",
+          "label": "Green"
+        },
+        "color": "positive"
+      }],
+      [{
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"3\"}",
+          "label": "White"
+        },
+        "color": "default"
+      },
+     {
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"4\"}",
+          "label": "Blue"
+        },
+        "color": "primary"
+      }]
+    ]
+  }
+
+    return(json.dumps(keyb))
+
+
+
+
 
 
 app = flask.Flask(__name__)
@@ -28,27 +78,28 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def processing():
-    
+
     data = json.loads(request.data)
     print(data)
-    
+
     if 'type' not in data.keys():
         return 'not vk'
-        
+
     if data['type'] == 'confirmation':
         return confirmation_token
-        
+
     elif data['type'] == 'message_new':
         session = vk.Session()
         api = vk.API(session, v=vk_api_version)
-        
+
         # User who calls bot
-        user_id = data['object']['user_id']
-        
-        api.messages.send(access_token=vk_token, user_id=str(user_id), message='Hello world!')
-        
-        
+        user_id = data['object']['from_id']
+
+        api.messages.send(access_token=vk_token, user_id=str(user_id), message='Hello world!', keyboard=menu_keyboard())
+
         return 'ok'
+
+
 
 
 
