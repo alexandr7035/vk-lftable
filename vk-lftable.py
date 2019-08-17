d9f36f4df8a64e0a4f25de316099ffb7a8350584
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-08-17 17:15:12 +0300

    Disable all notificaions if 'stop' button is pressed

--- vk-lftable.py
+++ vk-lftable.py
@@ -50,16 +50,16 @@ class LFTableBot():
         # VK
         session = vk.Session()
         self.api = vk.API(session, v=src.static.vk_api_version)
-        
+
         # Objects to access the databases
         self.timesdb = src.db_classes.TimesDB()
         self.notificationsdb = src.db_classes.NotificationsDB()
         self.statisticsdb = src.db_classes.StatisticsDB()
         self.clientsdb = src.db_classes.ClientsDB()
-        
+
         # Create necessary directories and files
         self.prepare_workspace()
-        
+
         # Sets times to the 'times.db' immediately after the run WITHOUT notifiying users
         # This is to prevent late notifications if the bot was down for a long time
         self.timesdb.connect()
@@ -67,26 +67,26 @@ class LFTableBot():
             update_time = src.gettime.ttb_gettime(timetable).strftime('%d.%m.%Y %H:%M:%S')
             self.timesdb.write_time(timetable.shortname, update_time)
         self.timesdb.close()
-        
-        
+
+
         # Timejob for notifications
         # Use 'atexit' to shut down the scheduler when exiting the app
         scheduler = BackgroundScheduler()
-        scheduler.add_job(func=self.notifications_timejob, 
-                          trigger="interval", 
+        scheduler.add_job(func=self.notifications_timejob,
+                          trigger="interval",
                           seconds=src.static.check_updates_interval)
         scheduler.start()
         atexit.register(lambda: scheduler.shutdown())
 
-        
+
     def prepare_workspace(self):
-        
+
         # Create directory for sqlite3 databases
         if not os.path.exists(src.static.db_dir):
             os.mkdir(src.static.db_dir)
-            
+
             logger.info("'" + src.static.db_dir + "' directory was created")
-            
+
         # Create databases. See db_classes.py
         if not os.path.isfile(src.static.clientsdb_path):
             self.clientsdb.connect()
@@ -115,21 +115,21 @@ class LFTableBot():
             self.statisticsdb.close()
 
             logger.info("'" + src.static.statisticsdb_path + "' database was created")
-        
+
     def handle_request(self, flask_request):
         data = json.loads(flask_request.data)
         print(data)
-        
+
         if 'type' not in data.keys():
             return 'not vk'
 
         # Send confirmation token to vk if requested
         if data['type'] == 'confirmation':
             return confirmation_token
-            
+
         # If got message from user
         elif data['type'] == 'message_new':
-            
+
             # Prevent answers to old requests if bot was down
             request_time = data['object']['date']
             requeste = data['object']['date']
@@ -140,12 +140,12 @@ class LFTableBot():
             # Get user id
             user_id = str(data['object']['from_id'])
             print(user_id)
-            
-            
+
+
             self.clientsdb.connect()
-            
+
             if self.clientsdb.check_if_user_is_client(user_id) is True:
-            
+
                 # That means a button was pressed
                 if data['object'].get('payload'):
                     callback = json.loads(data['object']['payload'])['button']
@@ -153,58 +153,58 @@ class LFTableBot():
                 # Usual message was sent
                 else:
                     print('receive usual message')
-                    self.send_message(user_id, 
-                                     src.messages.main_menu_text(), 
+                    self.send_message(user_id,
+                                     src.messages.main_menu_text(),
                                      src.keyboards.main_keyboard())
-                    
+
             else:
                 if data['object'].get('payload'):
                     callback = json.loads(data['object']['payload'])['button']
                     if callback == 'start':
                         # Add user to clients db
                         self.clientsdb.add_client(user_id)
-                        self.send_message(user_id, 
-                                          src.messages.main_menu_text(), 
-                                          src.keyboards.main_keyboard())                      
+                        self.send_message(user_id,
+                                          src.messages.main_menu_text(),
+                                          src.keyboards.main_keyboard())
                 else:
-                    self.send_message(user_id, 
-                                      src.messages.start_text(), 
+                    self.send_message(user_id,
+                                      src.messages.start_text(),
                                       src.keyboards.start_keyboard())
-            
+
             self.clientsdb.close()
-                
-             
+
+
         return 'ok'
-    
+
     def handle_button_callback(self, user_id, callback):
         print('CALLBACK')
-        
+
         if callback == 'main_menu':
             self.send_message(user_id, src.messages.main_menu_text(), src.keyboards.main_keyboard())
-        
+
         if callback == 'download':
             self.send_message(user_id, src.messages.download_text(), src.keyboards.download_keyboard())
-            
+
         if callback in ['pravo_menu', 'ek_polit_menu', 'mag_menu']:
             if callback == 'pravo_menu':
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.pravo_keyboard(user_id))
             elif callback == 'ek_polit_menu':
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.ek_polit_keyboard(user_id))
             elif callback == 'mag_menu':
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.mag_keyboard(user_id))
-        
+
         if  callback in ['pravo_c1', 'pravo_c2', 'pravo_c3', 'pravo_c4',
                           'ek_polit_c1', 'ek_polit_c2', 'ek_polit_c3', 'ek_polit_c4',
                         'mag_c1', 'mag_c2']:
-            
-            timetable = getattr(src.static, callback)                
-            
+
+            timetable = getattr(src.static, callback)
+
             self.notificationsdb.connect()
             if self.notificationsdb.check_if_user_notified(user_id, timetable.shortname) is True:
                 self.notificationsdb.disable_notifications(user_id, timetable.shortname)
@@ -213,34 +213,40 @@ class LFTableBot():
                 self.notificationsdb.enable_notifications(user_id, timetable.shortname)
                 self.send_message(user_id, src.messages.notification_enabled_text(timetable))
             self.notificationsdb.close()
-                
+
             if callback in ['pravo_c1', 'pravo_c2', 'pravo_c3', 'pravo_c4']:
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.pravo_keyboard(user_id))
             elif callback in ['ek_polit_c1', 'ek_polit_c2', 'ek_polit_c3', 'ek_polit_c4']:
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.ek_polit_keyboard(user_id))
             elif callback in ['mag_c1', 'mag_c2']:
-                self.send_message(user_id, 
-                                  src.messages.main_menu_text(), 
+                self.send_message(user_id,
+                                  src.messages.main_menu_text(),
                                   src.keyboards.mag_keyboard(user_id))
-                
-                
+
+
         if callback == 'stop':
+
+            self.notificationsdb.connect()
+            for timetable in src.static.all_timetables:
+                if self.notificationsdb.check_if_user_notified(user_id, timetable.shortname):
+                    self.notificationsdb.disable_notifications(user_id, timetable.shortname)
+
             self.clientsdb.connect()
             self.clientsdb.remove_client(user_id)
             self.send_message(user_id, src.messages.stop_text())
-            
-    
+
+
     def send_message(self, user_id, text, keyboard=None):
-        self.api.messages.send(access_token=vk_token, 
-                              user_id=user_id, 
-                              message=text, 
+        self.api.messages.send(access_token=vk_token,
+                              user_id=user_id,
+                              message=text,
                               keyboard=keyboard)
-                              
-                              
+
+
     def notifications_timejob(self):
         print('Checking for ttb updates was started: ', datetime.now().strftime("%d.%m.%Y %Y %H:%M:%S"))
 
@@ -276,7 +282,7 @@ class LFTableBot():
                 for user_id in users_to_notify:
 
                     try:
-                        self.send_message(user_id, 
+                        self.send_message(user_id,
                                           src.messages.notification_text(checking_ttb, dt_update_time),
                                           src.keyboards.notification_keyboard())
                     # If user blocked this bot & etc...
@@ -310,9 +316,9 @@ def main_handler():
 
 
 if __name__ == '__main__':
-    
+
     # Log message
     logger.info("the program was STARTED now")
-    
+
     app.run(host='127.0.0.1', port=5080, use_reloader=False)
-    
+
