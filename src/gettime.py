b8357bebee0c61a458f2cb42ce78acc9a36fbf1e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-05-10 16:01:49 +0300

    Add sepate credit_exam_gettime() function for credits and exams
    Credits and exams timetables may have 2 urls

--- src/gettime.py
+++ src/gettime.py
@@ -30,3 +30,58 @@ def ttb_gettime(ttb):
     date = old_tz.localize(gmt_date).astimezone(new_tz)
 
     return(date)
+
+
+# Sepatate function for cretis and exams
+# Credit/exam timetable may have 2 urls (Depends on season and course)
+# In this function we need to return both time and url (to use later in messages)
+def credit_exam_gettime(timetable):
+
+    # THIS IS A HOTFIX TO PREVENT "CERTIFICATE_VERIFY_FAILED" ERROR!
+    # DISABLE THIS LATER
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    # !!!!!
+    # See src.static.py 
+    # If there is winter and summer timetable files avaiable (course 1-3), get both, 
+    # compare and return the freshest 
+    # Else get only winter time (for course 4)
+    # !!!!!
+
+    old_tz = pytz.timezone('Europe/London')
+    new_tz = pytz.timezone('Europe/Minsk')
+
+    if len(timetable.urls) == 2:
+        winter_url = timetable.urls['winter']
+        summer_url = timetable.urls['summer']
+
+        # Get winter time
+        response =  urllib.request.urlopen(winter_url, timeout=25, context=ctx)
+        native_date = ' '.join(dict(response.headers)['Last-Modified'].rsplit()[1:-1])
+        gmt_date = datetime.strptime(native_date, '%d %b %Y %H:%M:%S')
+        winter_date = old_tz.localize(gmt_date).astimezone(new_tz)
+
+        # Get summer time
+        response =  urllib.request.urlopen(summer_url, timeout=25, context=ctx)
+        native_date = ' '.join(dict(response.headers)['Last-Modified'].rsplit()[1:-1])
+        gmt_date = datetime.strptime(native_date, '%d %b %Y %H:%M:%S')
+        summer_date = old_tz.localize(gmt_date).astimezone(new_tz)
+
+        # Choose relevant time and return
+        if winter_date > summer_date:
+            return({'time': winter_date, 'url': winter_url})
+        else:
+            return({'time': summer_date, 'url': summer_url})
+
+    else:
+        winter_url = timetable.urls['winter']
+
+        # Get only winter time
+        response =  urllib.request.urlopen(winter_url, timeout=25, context=ctx)
+        native_date = ' '.join(dict(response.headers)['Last-Modified'].rsplit()[1:-1])
+        gmt_date = datetime.strptime(native_date, '%d %b %Y %H:%M:%S')
+        winter_date = old_tz.localize(gmt_date).astimezone(new_tz)
+
+        return({'time': winter_date, 'url': winter_url})
